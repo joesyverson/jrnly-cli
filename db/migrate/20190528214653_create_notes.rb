2e5df05ae1e618a17fac4c84272a449d3a41a446
sticklercarla <carlastickler@gmail.com>
2019-05-28 17:55:20 -0400

    recreated migrations and moved app folder to lib

---
+++ db/migrate/20190528214653_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :notes do |t|
+      t.string  :title
+      t.string  :body
+      t.string  :link
+      # t.string  :img_url
+      t.string  :tag_id
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20190528214906_create_to_dos.rb
@@ -0,0 +1,12 @@
+class CreateToDos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :to_dos do |t|
+      t.string  :title
+      t.string  :description
+      t.string  :link
+      # t.string  :img_url
+      t.string  :tag_id
+      t.timestamps
+    end
+  end
+end
