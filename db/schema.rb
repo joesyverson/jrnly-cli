ba2a97abbe271b6885ea04d1aca77fbd0f64c49e
Joseph <josephgavinsyverson@gmail.com>
2019-05-28 15:17:43 -0400

    updated datatype for birthday column in user model

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 4) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "birthday"
+    t.string   "name"
+    t.datetime "birthday"
   end
 
 end
