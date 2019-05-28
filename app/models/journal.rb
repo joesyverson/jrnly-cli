c241951baa6a29ba7a9d021318865881c24d6567
Joseph <josephgavinsyverson@gmail.com>
2019-05-28 11:50:40 -0400

    described all remaining models and relationships

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,1 +1,7 @@
-class
+class Journal < ActiveRecord::Base
+
+  belongs_to :user
+  has_many :to_dos
+  has_many :notes
+
+end
---
+++ app/models/to_do.rb
@@ -0,0 +1,5 @@
+class ToDo < ActiveRecord::Base
+
+  belongs_to :journal
+
+end
