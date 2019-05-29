d15180d3cf1f38abef81271c5ffab8a408382dd4
sticklercarla <carlastickler@gmail.com>
2019-05-29 11:24:31 -0400

    added run file

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,28 @@
 require_relative '../config/environment'
 
 
-puts "Welcome to JRNLY, your new favorite journal app! Log in, or sign up!
+puts "Welcome to JRNLY, your new favorite journal app! Log in, or sign up!"
 
 prompt = TTY::Prompt.new
+
+yesorno = prompt.yes?('Are you a returning user?')
+
+
+# user puts in their information
+
+# automatically output today's messages
+
+# provides an option to create a new message
+
+# provides options to sort messages
+
+# provide an option for destroying and modifying a message
+    # "Would you like to change an existing message?"
+    # "Would you like to delete an existing message?"
+
+# exit
+
+
 
 
 
