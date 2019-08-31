b540291b6e297d21c33c07abf70bdaafbca2f811
Joseph G Syverson <josephgavinsyverson@gmail.com>
2019-08-31 14:50:45 -0400

    modified seed file for ease of demo and debugging

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 
 
-user1 = User.create(name: "Carla", password: "hello")
-user2 = User.create(name: "Joe", password: "hello")
-user3 = User.create(name: "Mark", password: "hello")
+user1 = User.create(name: "carla", password: "hello")
+user2 = User.create(name: "joe", password: "hello")
+user3 = User.create(name: "mark", password: "hello")
 
-tag1 = Tag.create(name: "Groceries")
-tag2 = Tag.create(name: "Chores")
-tag3 = Tag.create(name: "Work")
+tag1 = Tag.create(name: "groceries")
+tag2 = Tag.create(name: "chores")
+tag3 = Tag.create(name: "work")
 
 mes1 = Message.create(title: "title1", body: "the body", status: "Note", user_id: 1, tag_id: 2)
 mes1 = Message.create(title: "work!", body: "DO WORK", status: "To Do", user_id: 1, tag_id: 3)
